df58153a3bba9d329f7c89eb5879fc925de2b983
Frank W <frankwu26@gmail.com>
2018-08-22 13:22:58 -0400

    clean up

--- Scrapper/scrapper.js
+++ Scrapper/scrapper.js
@@ -1,3 +1,18 @@
-function bmo (){
-	
+//async scrape from various sites
+
+const PROMISES[];
+
+PROMISES.push(new Promise(resolve, reject){
+	resolve(bmo());
+});
+
+
+
+
+
+function bmo(){
+
+
+
+
 }
--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
+const scrape = require("./scrapper/scrapper.js");
+
+app.set("view engine", "ejs");
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-	res.render('index');
+	d = new Date();
+	res.render('index', {date: new Date().toDateString()});
+	next();
 })
+
+app.get('/', scrape);
+
 
 app.listen(8080, function(){
 	console.log("listening on port 8080");
 })
 
-app.set("view engine", "ejs");
-app.use(express.static('public'));
