42a30c879795b8e531af0140af7048ae5bcd8a86
Frank W <frankwu26@gmail.com>
2018-08-26 22:45:14 -0400

    successfully send scraped data to render function

--- Scrapper/scrapper.js
+++ Scrapper/scrapper.js
@@ -1,18 +1,95 @@
-//async scrape from various sites
+module.exports = {
+	scrape: (res) => {
+		const PROMISES = [];
 
-const PROMISES[];
+		PROMISES.push(new Promise((resolve, reject) => {
+			sb(resolve);
+		}));
 
-PROMISES.push(new Promise(resolve, reject){
-	resolve(bmo());
-});
-
-
-
-
+		Promise.all(PROMISES).then(values => {
+			//console.log(values);
+			res.render('index', {date: new Date().toDateString(), data: values});
+		});
+	}
+}
 
 function bmo(){
+	const cheerio = require("cheerio");
+	const rp = require("request-promise");
 
+	const options = {
+		uri: "http://www.scotiabank.com/rates/fxrates.html",
+		transform: function(body){
+			return cheerio.load(body);
+		}
+	}
 
+	rp(options)
+	.then(($) => {
+		console.log($.html());
 
+		// const currencies = $(".currency-col").contents().length;
+		// const buys = $(".buy-col").contents();
+		// const sell = $(".sell-col").contents();
+		// console.log(currencies);
+		// console.log(buys);
+		// console.log(sell);
+		//NEED SELENIUM
+	})
+	.catch((err) => {
+		console.log("BMO retrieve fail");
+		console.log(err);
+	});
+}
 
+function td(){
+	//sele
+}
+
+function rbc(){
+	//selenium
+}
+
+function sb(resolve){
+	const cheerio = require("cheerio");
+	const rp = require("request-promise");
+
+	const options = {
+		uri: "http://www.scotiabank.com/rates/fxrates.html",
+		transform: function(body){
+			return cheerio.load(body);
+		}
+	}
+	rp(options)
+	.then(($) => {
+		let data = [];
+		//console.log($("tbody").children().first().children().first().text());
+		$("tbody").children().each((i, elem) => {
+			const inner = $("tbody").children().eq(i).children();
+			data.push({
+				country: inner.eq(0).text(),
+				code: inner.eq(1).text(),
+				buy: inner.eq(2).text(),
+				sell: inner.eq(3).text()
+			});
+		});
+		resolve(data);
+		//return data;
+	})
+	.catch((err) => {
+		console.log("BMO retrieve fail");
+		console.log(err);
+	});
+}
+
+function dj(){
+	//normal scrape
+}
+
+function hsbs(){
+	//normal scrape
+}
+
+function uex(){
+	//normal
 }
--- server.js
+++ server.js
@@ -6,15 +6,11 @@ app.set("view engine", "ejs");
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-	d = new Date();
-	res.render('index', {date: new Date().toDateString()});
-	next();
-})
-
-app.get('/', scrape);
-
+	const d = scrape.scrape(res);
+	console.log("Waiting");
+});
 
 app.listen(8080, function(){
 	console.log("listening on port 8080");
-})
+});
 
